fca7c077276f450519082ad1630037e47f77ef6f
Alexandr Shubskij <alexandrshubskij@gmail.com>
2022-04-30 09:56:11 +0300

    Add remove post

--- maybe-new-fckng-project/src/components/PostItem.jsx
+++ maybe-new-fckng-project/src/components/PostItem.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from 'react';
+import MyButton from './UI/button/MyButton';
 
 const PostItem = (props) => {
 
@@ -11,7 +12,7 @@ const PostItem = (props) => {
 				<div>{props.post.body}</div>
 			</div>
 			<div className='post__btns'>
-				<button>Удалить</button>
+				<MyButton onClick={() => props.remove(props.post)}>Delete</MyButton>
 			</div>
 		</div>
 	);
